c76b0d8b2ec35a8214cc15b18187d419f442b41c
Lê Gia Phú <520H0401@student.tdtu.edu.vn>
2024-01-25 22:15:16 +0700

    add ContentsModel delete trigger

--- src/api/v1/models/ContentsModel.js
+++ src/api/v1/models/ContentsModel.js
@@ -1,3 +1,4 @@
+const createError = require("http-errors");
 const { default: mongoose } = require("mongoose");
 const Schema = require("mongoose").Schema;
 // Define ContentsModel:
@@ -9,5 +10,20 @@ const ContentsModel = new Schema(
   },
   { timestamps: true }
 );
+//Pre Operations (Triggers):
+const ServicesModel = require("../models/ServicesModel");
+ContentsModel.pre("findOneAndDelete", async function (next) {
+  try {
+    const contentId = this.getQuery()._id;
+    // Update all assignments with the current person's _id to remove the reference
+    await ServicesModel.updateMany(
+      { Contents: contentId },
+      { $pull: { Contents: contentId } }
+    );
+    return next();
+  } catch (err) {
+    return next(createError(500, err.message));
+  }
+});
 // Exports:
 module.exports = mongoose.model("ContentsModel", ContentsModel);
