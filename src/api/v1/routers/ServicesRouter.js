08e426ce1c36ab54e2806f31f9157f7009228181
Lê Gia Phú <520H0401@student.tdtu.edu.vn>
2024-01-26 11:03:35 +0700

    update error dir of service

--- src/api/v1/routers/ServicesRouter.js
+++ src/api/v1/routers/ServicesRouter.js
@@ -30,7 +30,7 @@ router
 // Contents Error Handling:
 router
   .use((req, res, next) => {
-    next(createError(404, "This /contents directory does not exist!"));
+    next(createError(404, "This /services directory does not exist!"));
   })
   .use((err, req, res, next) => {
     let errorStatus = err.status || 404;
